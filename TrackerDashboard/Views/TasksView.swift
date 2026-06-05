cc15b217ac935be0a5f0c5c66ca1f7abee421650
Christian <christian.kremer@whu.edu>
2026-06-05 15:51:14 +0200

    fix: limit tasks tab to current day

--- TrackerDashboard/Views/TasksView.swift
+++ TrackerDashboard/Views/TasksView.swift
@@ -5,7 +5,9 @@ struct TasksView: View {
     @State private var showingAddTask = false
 
     private var tasks: [ScheduleItem] {
-        sync.snapshot.openTasks.sorted {
+        sync.snapshot.openTasks
+            .filter { $0.date == sync.snapshot.date }
+            .sorted {
             ($0.adjustedPriority ?? -1, $0.priority ?? -1) >
             ($1.adjustedPriority ?? -1, $1.priority ?? -1)
         }
